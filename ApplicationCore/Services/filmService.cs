d25cb4214d4ffb6e27d53427b931ea1f2ed7aae5
slimayadi20 <slim.ayadi@esprit.tn>
2023-05-12 15:10:25 +0100

    first commit

---
+++ ApplicationCore/Services/filmService.cs
@@ -0,0 +1,11 @@
+﻿using AM.ApplicationCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+   
+}
